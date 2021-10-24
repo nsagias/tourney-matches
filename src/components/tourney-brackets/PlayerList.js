442209713d464a75cf540f7577840e76ad3ae6f8
Nick <nsagias@gmail.com>
2021-10-24 18:59:01 -0400

    add started code to component

--- src/components/tourney-brackets/PlayerList.js
+++ src/components/tourney-brackets/PlayerList.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Player from './Players';
 
 function PlayerList() {
-  <h1></h1>
+  <section className="PlayerList">
+    <h1>Current participating players</h1>
+    {/* Players will be shown here */}
+  </section>
 }
 
 export default Player;
