506bb85a7f9e328b7ae5042a5b834f6441e89978
Nick <nsagias@gmail.com>
2021-10-25 14:13:04 -0400

    update match list with keys to only change keys

--- src/components/tourney-brackets/MatchList.js
+++ src/components/tourney-brackets/MatchList.js
@@ -3,11 +3,14 @@ import matchData from "../../data/matchData";
 import Match from './Match';
 
 function MatchList(props) {
-  const oneMatch = matchData[3];
+  // const oneMatch = matchData[3];
+  // const parsedMatches = matchData.map(match => <Match {...match} />);
+  const parsedMatches = matchData.map(match => <Match key={match.matchNumber}{...match} />);
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      <Match {...oneMatch} />
+      {/* <Match {...oneMatch} /> */}
+      {parsedMatches}
     </section>
   );
 }
