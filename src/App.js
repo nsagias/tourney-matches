65b13a863bc5de8579f2890e20b08bf64dfb508c
Nick <nsagias@gmail.com>
2021-10-24 22:22:52 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MatchList from './components/tourney-brackets/MatchList';
+import PlayerList from './components/tourney-brackets/PlayerList';
+
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Tourney Matches <span>Where Coding and Tournaments found their Match!</span></h1>
+      <PlayerList />
+      <MatchList />
     </div>
   );
 }
