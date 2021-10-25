ec580c1d4e1d94e548618a01ebb6d9f56f5ed489
Nick <nsagias@gmail.com>
2021-10-25 00:31:25 -0400

    Add helper function addWinToPlayers

--- src/helpers/playerHelpers.js
+++ src/helpers/playerHelpers.js
@@ -1,8 +1,24 @@
-export const preparePlayerData = () => {
-
+export const preparePlayerData = (playerdata) => {
+  return  Object.values(playerdata);
 }
 
-export const addWinsToPlayers = () => {
+  // input: playerDataArray, matchData
+  export const addWinsToPlayers = (playerDataArray, matchData) => {
+    return playerDataArray.map(player => {
+      // Calculating the number of wins in the matchData
+      const currentWins = matchData.reduce((accumulator, match) => {
+        // Adds a win if the gamerTag matches
+        if (match.winner === player.gamerTag) {
+          return accumulator += 1;
+        } else {
+          return accumulator;
+        }
+      }, 0);
 
-  
-}
+      // Assigns the value to the wins key
+      player.wins = currentWins;
+
+      return player
+    });
+  }
+  // output: [{player}, {player}] (each player having a win key and a numerical value)
